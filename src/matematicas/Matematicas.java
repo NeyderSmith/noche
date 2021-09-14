22f3012cc7f4cc0cbe28fc70a241fff864d0b059
ASUS <ASUS@LAPTOP-DN9Q5Q6U.lan>
2021-09-14 01:43:54 -0500

    inicio matematicas

--- src/matematicas/Matematicas.java
+++ src/matematicas/Matematicas.java
@@ -1,21 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package matematicas;
 
-/**
- *
- * @author ASUS
- */
+package matematicas;
+import java.util.Scanner;
+
 public class Matematicas {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner entrada= new Scanner (System .in);
+    float numero1,numero2,suma,resta,div,mult,resto;
+    
+        System.out.println("digite dos numeros: ");
+         numero1 = entrada.nextFloat();
+         numero2 = entrada.nextFloat();
+         
+        suma=numero1+numero2;
+        resta=numero1-numero2;
+        mult=numero1*numero2;
+        div=numero1/numero2;
+        resto = numero1%numero2;
+        
+        System.out.println("la suma es: "+suma);
+        System.out.println("la resta es: "+resta);
+        System.out.println("la multiplicaciones: "+mult);
+        System.out.println("la division es: "+div);
+        System.out.println("el residuo es: "+resto);
+       
     }
     
 }
